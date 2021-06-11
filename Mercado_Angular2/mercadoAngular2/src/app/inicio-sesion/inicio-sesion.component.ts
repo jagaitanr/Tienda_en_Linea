8dd7649f779bfcc2a12d0c7abe27fbad01f2ceb0
jagaitanr <jagaitanr@hotmail.com>
2021-06-11 12:41:23 -0500

    version Junio 11 de 2021

--- Mercado_Angular2/mercadoAngular2/src/app/inicio-sesion/inicio-sesion.component.ts
+++ Mercado_Angular2/mercadoAngular2/src/app/inicio-sesion/inicio-sesion.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-//import {InicioSesionComponent} from './inicio-sesion.component'
-import {
-  FormGroup,
-  FormBuilder,
-  FormControl,
-  Validators
-} from "@angular/forms";
+import {  FormGroup,   FormBuilder,   FormControl,   Validators } from "@angular/forms";
 
-import {userAsyncValidator } from './ValidatorsForms/usuario.directive';
-import {UserService } from './Services/user.service';
+//import {userAsyncValidator } from './ValidatorsForms/usuario.directive';
+//import {UserService } from './Services/user.service';
 
 
 @Component({
@@ -17,24 +11,27 @@ import {UserService } from './Services/user.service';
   styleUrls: ['./inicio-sesion.component.css']
   
 })
+
 export class InicioSesionComponent implements OnInit {
 
   titulo = 'esta es mi página de inicio de sesion';
-
   nombreUsuario = 'PepitoPerez';
   contraseña  = '12345';
 
   
-  public loginForm:FormGroup;
-  constructor(private fb:FormBuilder) {
+    //  public loginForm:FormGroup;
+    /* constructor(private fb:FormBuilder) {
     
-   }
-
-  public ngOnInit() {
-    this.loginForm = this.fb.group({
-      usuario: new FormControl("", [Validators.required], [userAsyncValidator(this.userService)]),
-      email: new FormControl("",[Validators.required,Validators.email]),
-      password: new FormControl("",[Validators.required,Validators.minLength(6)])
+    }
+    */
+    myForm: FormGroup;
+    constructor(public fb:FormBuilder)
+    {
+      this.myForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      });
+    }
+    ngOnInit() {}
+    saveData(){console.log(this.myForm.value);} 
   }
- public sendLogin(){}
-}
