19efcf433672c4f3ec1acfd20e5cfaec8cbee111
Jaime <jagaitanr@hotmail.com>
2021-07-21 22:13:24 -0500

    creacion componente barra superior

--- Mercado_Angular2/mercadoAngular2/src/app/app.routing.ts
+++ Mercado_Angular2/mercadoAngular2/src/app/app.routing.ts
@@ -3,11 +3,13 @@ import {RouterModule} from  '@angular/router';
 import {AppComponent} from './app.component';
 import {PaginaPrincipalComponent} from './pagina-principal/pagina-principal.component';
 import {InicioSesionComponent} from './inicio-sesion/inicio-sesion.component';
+import { BarraSuperiorComponent } from './barra-superior/barra-superior.component';
 
 
 const appRoutes = [
     {path:'pagina-principal', component: PaginaPrincipalComponent, pathMatch: 'full'},
-     {path:'inicio-sesion', component: InicioSesionComponent,  pathMatch: 'full'}
+     {path:'inicio-sesion', component: InicioSesionComponent,  pathMatch: 'full'},
+     {path:'barra-superior', component: BarraSuperiorComponent,  pathMatch: 'full'}
 ];
 
 export const routing = RouterModule.forRoot (appRoutes);
