2cd33034418fa987b95fbc063712280ea2b707a9
Jaime <jagaitanr@hotmail.com>
2021-09-08 16:21:57 -0500

    ´pagina catalogo 60%

--- Mercado_Angular2/mercadoAngular2/src/app/catalogo/catalogo.component.ts
+++ Mercado_Angular2/mercadoAngular2/src/app/catalogo/catalogo.component.ts
@@ -3,6 +3,7 @@ import * as data from '../../assets/productos.json';
 import { UserService } from 'app/Services/user.service';
 import { DetalleproductoComponent } from 'app/detalleproducto/detalleproducto.component';
 import { VariablesGlobales } from 'app/common/Variables-globales';
+import { summaryFileName } from '@angular/compiler/src/aot/summary_serializer';
 
 
 
@@ -29,23 +30,31 @@ export class CatalogoComponent{
   }
 
   AgregandoProductos(NumerodeProductos, nombreProducto){
-          
+    var yaEstaProducto = false;   
     for (let i=0; i<=VariablesGlobales.productosCanasta; i++){  //recorre el arreglo para revisar si ya esta en la canasta el producto si es así simplemente suma la cantidad a añadir
         if (VariablesGlobales.productosAñadidosNombre[i]===nombreProducto){
-            VariablesGlobales.productosAñadidosCantidad[i]=VariablesGlobales.productosAñadidosCantidad[i] + NumerodeProductos;
+            
+          VariablesGlobales.productosAñadidosCantidad[i] = Number(VariablesGlobales.productosAñadidosCantidad[i]) + Number(NumerodeProductos);
+          
             console.log("dentro del if "+ i);
-           VariablesGlobales.productosCanasta=VariablesGlobales.productosCanasta-1;
-        }
-    else {
-          VariablesGlobales.productosAñadidosNombre[VariablesGlobales.productosCanasta]=nombreProducto;
-          VariablesGlobales.productosAñadidosCantidad[VariablesGlobales.productosCanasta]=NumerodeProductos
-          console.log("dentro del else "+ i);
+           i=VariablesGlobales.productosCanasta+1;//para sacarlo de for ya que encontró el mismo producto y no es necesario revisar más
+            yaEstaProducto=true; // si encuentra un producto igual
           }
+      if (yaEstaProducto) {
+          //VariablesGlobales.productosAñadidosNombre[VariablesGlobales.productosCanasta]=nombreProducto;
+          VariablesGlobales.productosAñadidosCantidad[i]=Number(VariablesGlobales.productosAñadidosCantidad)+Number(NumerodeProductos); //agrega la cantidad de productos solamente
+          console.log("dentro de ya esta producto "+ i);
+          }
+    }
+      if (!yaEstaProducto){
+        VariablesGlobales.productosAñadidosNombre[VariablesGlobales.productosCanasta]= nombreProducto;
+        VariablesGlobales.productosAñadidosCantidad[VariablesGlobales.productosCanasta]= NumerodeProductos;
+        VariablesGlobales.productosCanasta=VariablesGlobales.productosCanasta+1
+  }
         console.log (VariablesGlobales.productosAñadidosNombre); 
         console.log (VariablesGlobales.productosAñadidosCantidad); 
         console.log (VariablesGlobales.productosCanasta);
-    }
-    VariablesGlobales.productosCanasta=VariablesGlobales.productosCanasta+1;
+    
     
   }  
 
