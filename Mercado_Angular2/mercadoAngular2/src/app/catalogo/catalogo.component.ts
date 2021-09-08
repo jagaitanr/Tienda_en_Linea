4417bcab7e006ca3afeea8c5688f78c2bad4d88f
Jaime <jagaitanr@hotmail.com>
2021-09-07 22:00:06 -0500

    iteracion for para arreglo de productos en canasta-50%

--- Mercado_Angular2/mercadoAngular2/src/app/catalogo/catalogo.component.ts
+++ Mercado_Angular2/mercadoAngular2/src/app/catalogo/catalogo.component.ts
@@ -29,18 +29,24 @@ export class CatalogoComponent{
   }
 
   AgregandoProductos(NumerodeProductos, nombreProducto){
-    for (let i in VariablesGlobales.productosAñadidosNombre){  //recorre el arreglo para revisar si ya esta en la canasta el producto si es así simplemente suma la cantidad a añadir
-        if (VariablesGlobales.productosAñadidosNombre==nombreProducto){
+          
+    for (let i=0; i<=VariablesGlobales.productosCanasta; i++){  //recorre el arreglo para revisar si ya esta en la canasta el producto si es así simplemente suma la cantidad a añadir
+        if (VariablesGlobales.productosAñadidosNombre[i]===nombreProducto){
             VariablesGlobales.productosAñadidosCantidad[i]=VariablesGlobales.productosAñadidosCantidad[i] + NumerodeProductos;
+            console.log("dentro del if "+ i);
+           VariablesGlobales.productosCanasta=VariablesGlobales.productosCanasta-1;
         }
     else {
           VariablesGlobales.productosAñadidosNombre[VariablesGlobales.productosCanasta]=nombreProducto;
           VariablesGlobales.productosAñadidosCantidad[VariablesGlobales.productosCanasta]=NumerodeProductos
-          console.log (VariablesGlobales.productosAñadidosNombre); 
-          console.log (VariablesGlobales.productosAñadidosCantidad); 
-          VariablesGlobales.productosCanasta=VariablesGlobales.productosCanasta+1;
-        }
+          console.log("dentro del else "+ i);
+          }
+        console.log (VariablesGlobales.productosAñadidosNombre); 
+        console.log (VariablesGlobales.productosAñadidosCantidad); 
+        console.log (VariablesGlobales.productosCanasta);
     }
+    VariablesGlobales.productosCanasta=VariablesGlobales.productosCanasta+1;
+    
   }  
 
 
