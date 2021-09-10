cdbf6b5e5db218d0c521337f605ee2cdec70a454
Jaime <jagaitanr@hotmail.com>
2021-09-10 16:09:21 -0500

    desarrollando añadir producto modificando archivo json 10%

--- Mercado_Angular2/mercadoAngular2/src/app/catalogo/catalogo.component.ts
+++ Mercado_Angular2/mercadoAngular2/src/app/catalogo/catalogo.component.ts
@@ -4,6 +4,10 @@ import { UserService } from 'app/Services/user.service';
 import { DetalleproductoComponent } from 'app/detalleproducto/detalleproducto.component';
 import { VariablesGlobales } from 'app/common/Variables-globales';
 import { summaryFileName } from '@angular/compiler/src/aot/summary_serializer';
+//import { fstat } from 'fs';
+import { readFile } from 'fs';
+
+
 
 
 
@@ -26,30 +30,66 @@ export class CatalogoComponent{
     VariablesGlobales.detalle_producto=nombre;
     VariablesGlobales.pagina_actual = "PagDetalleProducto";
   
- 
-  }
+   }
+  ActualizarJson(cantidad, nombre){
+    //var jsonfile = require ('jsonfile');
+    var jsonfile = require ('fs');
+    var file = 'assets/productos.json';
+    var fs = require('fs');
+    var params = {
+    "nombre":"remolacha"
+    }
+    changeJson(3,params)//Ejecutalo;
 
-  AgregandoProductos(NumerodeProductos, nombreProducto){
+function changeJson(id,params){
+    jsonfile.readFile(file,function(err,data){
+        if(err){
+            console.error(err);
+        }
+        var productosTemp = data.toString(); // archivo Json temporal
+        productosTemp = JSON.parse(productosTemp);
+        // Leer los datos y modificarlos
+        for(var i = 0; i < productosTemp.data.length;i++){
+            if(id == productosTemp.data[i].id){
+                console.log('id lo mismo');
+                for(var key in params){
+                    if(productosTemp.data[i][key]){
+                        productosTemp.data[i][key] = params[key];
+                    }
+                }
+            }
+        }
+        productosTemp.total = productosTemp.data.length;
+        var str = JSON.stringify(productosTemp);
+        //console.log(str);
+        fs.writeFile(file,str,function(err){
+            if(err){
+                console.error(err);
+            }
+            console.log('-------------------- Modificado con éxito');
+            console.log(productosTemp.data);
+        })
+    })
+}
+        }
+ 
+ 
+  AgregandoProductos(NumerodeProductos, nombreProducto, unidades_disponibles){
     var yaEstaProducto = false;   
     for (let i=0; i<=VariablesGlobales.productosCanasta; i++){  //recorre el arreglo para revisar si ya esta en la canasta el producto si es así simplemente suma la cantidad a añadir
         if (VariablesGlobales.productosAñadidosNombre[i]===nombreProducto){
             
           VariablesGlobales.productosAñadidosCantidad[i] = Number(VariablesGlobales.productosAñadidosCantidad[i]) + Number(NumerodeProductos);
-          
-            console.log("dentro del if "+ i);
+          this.ActualizarJson(VariablesGlobales.productosAñadidosCantidad[i], nombreProducto);
+          console.log("dentro del if "+ i);
            i=VariablesGlobales.productosCanasta+1;//para sacarlo de for ya que encontró el mismo producto y no es necesario revisar más
-            yaEstaProducto=true; // si encuentra un producto igual
+           yaEstaProducto=true; // si encuentra un producto igual
           }
-      if (yaEstaProducto) {
-          //VariablesGlobales.productosAñadidosNombre[VariablesGlobales.productosCanasta]=nombreProducto;
-          VariablesGlobales.productosAñadidosCantidad[i]=Number(VariablesGlobales.productosAñadidosCantidad)+Number(NumerodeProductos); //agrega la cantidad de productos solamente
-          console.log("dentro de ya esta producto "+ i);
-          }
-    }
+      }
       if (!yaEstaProducto){
         VariablesGlobales.productosAñadidosNombre[VariablesGlobales.productosCanasta]= nombreProducto;
-        VariablesGlobales.productosAñadidosCantidad[VariablesGlobales.productosCanasta]= NumerodeProductos;
-        VariablesGlobales.productosCanasta=VariablesGlobales.productosCanasta+1
+        VariablesGlobales.productosAñadidosCantidad[VariablesGlobales.productosCanasta]= Number(NumerodeProductos);
+        VariablesGlobales.productosCanasta=Number(VariablesGlobales.productosCanasta)+1;
   }
         console.log (VariablesGlobales.productosAñadidosNombre); 
         console.log (VariablesGlobales.productosAñadidosCantidad); 
