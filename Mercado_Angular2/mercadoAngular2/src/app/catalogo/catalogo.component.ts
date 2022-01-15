0b255cc95f212ec95b3706e0d5c7b0623842db1f
Jaime <jagaitanr@hotmail.com>
2022-01-15 09:12:32 -0500

    arreglando el almacenamiento en base de datos

--- Mercado_Angular2/mercadoAngular2/src/app/catalogo/catalogo.component.ts
+++ Mercado_Angular2/mercadoAngular2/src/app/catalogo/catalogo.component.ts
@@ -84,6 +84,12 @@ export class CatalogoComponent implements OnDestroy, OnInit{
       console.log("la base de datos es: " + this.vegetales);
   }
 
+  habilitarFiltroFunct(){
+    console.log("entro a la habilitación del filtrrro");
+    this.habilitarFiltro=true;
+    this.habilitarFiltro2=false;
+  }
+
   listadeProductos = this.dataService.getVegetales();
    listaArray : any[]=Array.of(this.listadeProductos);
    listaArray2 = this.listaArray[0]; //se toma la posición 0 ya que al pasar el json a array, en la posición 0 es donde se desglosa el resto de datos
@@ -140,33 +146,32 @@ function changeJson(id,params){
  
   agregarProducto(imagenProducto, nombreProducto, precioProducto, unidadesDisponibles, posicionProducto, unidadesApartadas)
   {
- try{
-    //let  productosAgregadosNombre: string []=[];
-      let posicionInt = parseInt(posicionProducto);
+ try{  //se declaran variables tipo let para luego almacenar cada propiedad del producto en el localStorage
+      let posicionInt = parseInt(posicionProducto);//se pasa a entero y sera el pos-indice de cada parámetro
       let nombre:string='ProductosApartadosNombre'+ String(posicionInt);//se prepara para crear una variable localhost
       let precio:string='ProductosApartadosPrecio'+ String(posicionInt);
       let unidadesDispo:string='ProductosApartadosUniDisponibles'+ String(posicionInt);
       let unidadesApart:string='ProductosApartadosUniApartadas'+ String(posicionInt);
       let imagen:string='ProductosApartadosImagen'+ String(posicionInt);
       let enCanasta:string = 'ProductosApartadosenCanasta'+ String(posicionInt);
-      console.log ("el item es: "+posicionInt + posicionProducto);
+      console.log ('el item es: '+ String(posicionProducto));
       console.log ('la imagen a agregar es: '+ imagenProducto);
-      console.log ('este articulo esta en canasta?: '+ localStorage.enCanasta);
+      console.log ('este articulo esta en canasta?: '+ localStorage.getItem(enCanasta));
       console.log ('el nombre del producto a agregar es : ' + nombreProducto);
       console.log ('el precio del producto a agregar es: ' + precioProducto);
       console.log ('las unidades disponibles del producto a agregar es: ' + unidadesDisponibles);
-      console.log ('la posición en el catalogo es: ' + posicionInt);
+      console.log ('la posición en el catalogo es: ' + posicionProducto);
       
       localStorage.setItem(nombre,nombreProducto);
       localStorage.setItem(precio,precioProducto);
       localStorage.setItem(unidadesDispo,unidadesDisponibles);
       //localStorage.setItem(unidadesApart,unidadesApartadas);
       localStorage.setItem(imagen,imagenProducto);
-      localStorage.setItem('ProductosApartdosPosicion'+String(posicionInt),posicionProducto);
-      let f = parseInt(localStorage.getItem('productosenCanasta'));
+      localStorage.setItem('ProductosApartadosPosicion'+String(posicionInt),posicionProducto);
+      let f = parseInt(localStorage.getItem('CantidadproductosenCanasta'));
       
       
-      if (localStorage.getItem(enCanasta)==='true'){ //si es verdadero se sumara al actual unidades apartadas
+      if (localStorage.getItem(enCanasta)=='true'){ //si es verdadero se sumara al actual unidades apartadas
         var a = parseInt(localStorage.getItem(unidadesApart))+parseInt(unidadesApartadas);
         localStorage.setItem(unidadesApart,String(a)); //
         console.log ('las unidades apartadas para este producto son: ' + localStorage.getItem(unidadesApart));
@@ -175,7 +180,7 @@ function changeJson(id,params){
     
       else {
           f=f+1;
-          localStorage.setItem('productosenCanasta', String(f));
+          localStorage.setItem('CantidadproductosenCanasta', String(f));
           console.log('productos variados: '+ String(f)); 
            localStorage.setItem(enCanasta,'true'); //se registra que ya está en canasta
            console.log("entro al else"); //de lo contrario se iniciaran las variables en la posicion correspondiente
